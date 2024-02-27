b21feddfe81056f2be205b51c72fb26234a24037
Ricardopbm <160974223+Ricardopbm@users.noreply.github.com>
2024-02-26 21:32:08 -0300

    Update Votos.java

--- Ricardo TADS/Votos.java
+++ Ricardo TADS/Votos.java
@@ -1,45 +1,52 @@
 import java.util.Scanner;
 
-public class Votos {
+public class Evento {
     public static void main(String[] args){
 
         
         Scanner input = new Scanner(System.in); 
 
         
-        double ne; 
-        double nv;
-        double nb;
-        double nn;
+        double vi; //feito
+
+        double vid;
+
+        double ns1;
+        double ns;
+
+        double nc1; 
+        double nc;
+
+        double np1;
+        double np;
 
 
-        System.out.println("Digite o número total de eleitores: ");
-        ne = input.nextInt();
 
-        System.out.println("Digite o número de votos válidos: ");
-        nv = input.nextInt();
+        System.out.println("Insira o valor do ingresso inteiro: ");
+        vi = input.nextInt();
+        
 
-        System.out.println("Digite o número de votos em branco: ");
-        nb = input.nextInt();
+        System.out.println("Insira o número de sócios participando do evento: ");
+        ns1 = input.nextInt();
+        ns = vi * 0.3 * ns1;
 
-        System.out.println("Digite o número de votos nulos: ");
-        nn = input.nextInt();
+        System.out.println("Insira o número de crianças participando do evento: ");
+        nc1 = input.nextInt();
+        nc = nc1 * vi;
 
-        double pv;
-        double pb;
-        double pn;
+        System.out.println("Insira o número de pessoas acima de 10 anos, não sócios, participando do evento: ");
+        np1 = input.nextInt();
+        np = np1 * vi;
 
-        pv = nv / ne;
-        pb = nb / ne;
-        pn = nn / ne;
 
-        double pvp = pv * 100; 
-        double pbp = pb * 100;
-        double pnp = pn * 100;
+        double publicot = ns1 + nc1 + np1;
+        double renda = ns + np;
+        double deficit = ns1 * vi + nc + np;
 
-        System.out.print("O percentual de votos válidos é: " + pvp + "% \n");
-        System.out.print("O percentual de votos em branco é: " + pbp + "% \n");
-        System.out.print("O percentual de votos nulos é: " + pnp + "% \n");
+        System.out.println("O Público total foi de: " + publicot + " de pessoas.");
+        System.out.println("A renda total foi de: " + renda + "R$.");
+        System.out.println("O valor que deixou de ser adiquirido, devido a descontos e isenções foi de: " + deficit + "R$");
+
 
 
     }
